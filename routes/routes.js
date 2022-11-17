bfb78c40fe18024fa08a1a84c9b72b5901d90274
Ansh Goyal <110151117+AnshGoyal27@users.noreply.github.com>
2022-11-17 23:33:31 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -96,7 +96,7 @@ router.post('/payment',async (req,res)=>{
         success_url: succurl,
         cancel_url: `https://musicstorebm.netlify.app`,
     });
-    res.header("Access-Control-Allow-Origin","http://localhost:3000");
+    res.header("Access-Control-Allow-Origin","https://musicstorebm.netlify.app");
     res.header('Access-Control-Allow-Credentials', 'true')
     res.header("Access-Control-Allow-Methods","GET,POST");
     res.header("Access-Control-Allow-Headers","Origin,Content-Type,Authorization");
